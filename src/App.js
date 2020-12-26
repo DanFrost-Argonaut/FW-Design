25833392e20b2c0235f61d6267c095fd62a429b3
Dan Frost <dan.frost.code@gmail.com>
2020-12-26 11:40:35 -0500

    Added Button + Style
    Added Button.js and Button.css
    Added basic styling to nav and button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <>
       <Router>
         <Navbar />
         <Switch>
           <Route path='/' exact/>
         </Switch>
       </Router>
-    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { Button } from './Button'; 
+import Dropdown from './Dropdown';
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(!false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton); 
   return (
     <>
      <nav className="navbar">
@@ -10,10 +27,33 @@ function Navbar() {
          <Link to='/' className="navbar-logo">
            Frozen Waves Design {/* Replace with PNG*/}
          </Link>
-         <div className="menu-icon">
+         <div className="menu-icon" onClick={handleClick}>
            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
          </div>
-       </div>
+         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/Branding' className='nav-links' onClick={closeMobileMenu}>
+              Branding
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/WebDesign' className='nav-links' onClick={closeMobileMenu}>
+              Web Design
+            </Link>
+            </li>
+            <li className='nav-item'>
+            <Link to='/Advertising' className='nav-links' onClick={closeMobileMenu}>
+              Advertising
+            </Link>
+            </li>
+            <li className='nav-item'>
+            <Link to='/Design' className='nav-links' onClick={closeMobileMenu}>
+              Graphic Design
+            </Link>
+          </li>
+         </ul>
+          <Button />
+          </div>
      </nav>
     </>
   );
